4d02d6bbba30b6b1873150b0c1e4ba0a927876a3
Jaime Castrillón <jaimecastrillon@outlook.com>
2020-04-11 12:57:28 -0500

    Manejo de datos
    Manejo de datos
    Tarea

---
+++ data/array.js
@@ -0,0 +1,11 @@
+module.exports = {
+  intArray : [ 1, 2, 3, 2, 5, 2 ],
+  floatArray : [ 1.0, 2.1, 3.5 ],
+  stringArray : [ 'a', 'b', 'c' ],
+  jsonArray : [
+    { key: 'value1', id: 1},
+    { key: 'value2', id: 2},
+    { key: 'value3', id: 3},
+    { key: 'value1', id: 4}
+  ]
+}
---
+++ datos.js
@@ -0,0 +1,38 @@
+const array = require('./data/array')
+const intArray = array.intArray;
+const jsonArray = array.jsonArray;
+
+console.log(intArray);
+console.log(intArray.join(', '));
+console.log(intArray.join(':'));
+
+intArray.push(4);
+
+console.log(intArray.join('-'));
+intArray.map(i => {
+  console.log(i * 2)
+});
+//const first = intArray.shift();
+//const last = intArray.pop();
+
+//console.log(first)
+//console.log(last)
+//console.log(intArray)
+
+const filter = intArray.filter(i => i > 2);
+console.log(filter);
+
+const value = intArray.find(i => i == 2);
+console.log('Value: ', value);
+
+console.log('intArray: ', intArray);
+
+intArray.splice(4, 1);
+
+console.log('intArray: ', intArray);
+const json = jsonArray.find(i => i.key == 'value1');
+console.log(json);
+const jsonFilter = jsonArray.filter(i => i.key == 'value1');
+console.log(jsonFilter);
+jsonArray.push({ key: 'value5', id: 5});
+console.log(jsonArray);
