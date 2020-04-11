7779b516b75922dc153f105fd3bd4493c9a50afb
Jaime Castrillón <jaimecastrillon@outlook.com>
2020-04-11 10:50:19 -0500

    Clase del sábado
    Ejemplos de clases

--- es6.js
+++ es6.js
@@ -1,5 +1,52 @@
 /**
  * Ejercicios de ES6
+ * https://github.com/grupogenesys/curso-desarrollo
  */
 
+  let myVar = '';
+
+  const fn = (params) => {
+    // TODO: Actions
+  }
+
+
+ class Person {
+
+  constructor(name = '') {
+    this.name = name
+    this.email = ''
+    this.phone = ''
+    this.birthDay = ''
+    this.kmRuned = 1
+  }
+
+  run() {
+    return "I am running...";
+  }
+
+  static speak() {
+    return "I am speaking...";
+  }
+
+ }
+
+ const person = new Person('Juan');
+
+
+ class Teacher extends Person {
+  
+  constructor() {
+    super('Jaime')
+    this.job = 'Teach'
+  }
+
+  read() {
+    return 'I am reading ...';
+  }
+
+ }
  
+ const teacher = new Teacher()
+
+ console.log(teacher.name);
+ console.log(Teacher.speak());
