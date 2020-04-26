e964d72d2a5e4b15462ecc3353c0f6eb3e93090d
Jaime Castrillón <jaimecastrillon@outlook.com>
2020-04-26 16:54:14 -0500

    Validación request
    Se agrega validación a los request de auth y brands

--- routing/routes/brands.js
+++ routing/routes/brands.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 const DB = require('../../db/db')
 const db = new DB('BRANDS')
+const validator = require('../../validate/validator')
 
 router
   .get('/brands', async (req, res) => {
@@ -25,9 +26,14 @@ router
   })
   .post('/brands', async (req, res) => {
     try {
-      const data = req.body
-      const response = await db.create(data)
-      res.json({ status: 'ok', response })
+      const data = validator('brand').cleanData(req.body)
+      const errors = validator('brand').isValid(data)
+      if(errors) {
+        res.json({ status: 'error', error: errors })
+      } else {
+        const response = await db.create(data)
+        res.json({ status: 'ok', response })
+      }
     } catch(error) {
       res.json({ status: 'error', error })
     }
